b8bb229dbb4aabffe23bd15df3836b37f05321b5
takamandre <andre@pluriel.tech>
2025-10-04 04:29:17 +0100

    add response on path / for hash generator project

--- hash-generator/main.go
+++ hash-generator/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func generateHash() string {
@@ -19,9 +23,16 @@ func generateHash() string {
 func handler(w http.ResponseWriter, r *http.Request) {
 	hash := generateHash()
 	log.Printf("%s", hash)
+	w.Write([]byte(hash))
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	port := os.Getenv(("PORT"))
+	log.Printf("Starting TODO app on :%s", port)
+
 	ticker := time.NewTicker(5 * time.Second)
 	go func() {
 		for range ticker.C {
@@ -31,5 +42,5 @@ func main() {
 	}()
 
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
